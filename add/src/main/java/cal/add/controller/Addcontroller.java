ab143e2538f75567dccc011d911b5448bab07192
JyZo <jjy3680@naver.com>
2020-11-13 01:21:54 +0900

    config commit

--- add/src/main/java/cal/add/controller/Addcontroller.java
+++ add/src/main/java/cal/add/controller/Addcontroller.java
@@ -1,6 +1,6 @@
 package cal.add.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
