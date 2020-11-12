b0a75ab2ce47ce728b965050bc67a45fb2bff412
JyZo <jjy3680@naver.com>
2020-11-13 01:52:50 +0900

    add controller change

--- add/src/main/java/cal/add/controller/Addcontroller.java
+++ add/src/main/java/cal/add/controller/Addcontroller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Addcontroller {
 	@GetMapping("/adds")
 	public String home() {
-		return "index.html";
+		return "add test";
 	}
 	
 	}
