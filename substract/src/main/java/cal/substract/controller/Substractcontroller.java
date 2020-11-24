ca651afa988395090dd86508eef73a7c097e17eb
JyZo <jjy3680@naver.com>
2020-11-24 10:39:31 +0900

    substract method

--- substract/src/main/java/cal/substract/controller/Substractcontroller.java
+++ substract/src/main/java/cal/substract/controller/Substractcontroller.java
@@ -2,15 +2,33 @@ package cal.substract.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/cal")
+@RequestMapping("/subs")
 public class Substractcontroller {
-	@GetMapping("/subs")
+	@GetMapping("/test")
 	public String home() {
 		return "substract test";
 	}
 	
+	@GetMapping("/sub")
+	public Object add(@RequestParam(value="num1",required = false, defaultValue = "1")int num1,
+			@RequestParam(value="num2",required = false,defaultValue = "1")int num2) {
+		
+		if(num1>=num2) {
+			Result result = new Result(num1,num2);
+			return "Substract:"+result;
+		}else if(num1<num2) {
+			int temp=0;
+			temp=num1;
+			num1=num2;
+			num2=temp;
+			Result result = new Result(num1,num2);
+			return "Substract:"+result;
+		}
+		return null;
 	}
-
+}
